28cd3925c4ecf227795c2191c4be1937a9256514
luispaulo21 <luispaulocavalcante20@gmail.com>
2021-10-13 14:54:39 -0300

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function exibirRedesMobile() {
+    const botaoExibirRedesMobile = document.querySelector('.redes__mobile button');
+
+    const exibirRedes = (event) => {
+        const redes = botaoExibirRedesMobile.previousElementSibling;
+        redes.classList.toggle('ativo');
+        event.currentTarget.classList.toggle('ativo');
+    };
+
+    botaoExibirRedesMobile.addEventListener('click', exibirRedes);
+}
+
+exibirRedesMobile();
+
+function animacaoAoScroll() {
+    const secoes = document.querySelectorAll('.anim');
+
+    if(secoes.length) {
+        const exibirSecao = () =>{
+            const larguraDaTela = window.innerWidth;
+            let metadeDaTela = (larguraDaTela < 767) ? window.innerHeight * .3 : window.innerHeight * .5;
+
+            secoes.forEach((secao) => {
+                const caracteristicasDaSecao = secao.getBoundingClientRect().top;
+                if(caracteristicasDaSecao < metadeDaTela) {
+                    secao.classList.add('ativo');
+                }
+                // } else if(larguraDaTela < 767) {
+                //     metadeDaTela = window.innerHeight * .1;
+                else {
+                    secao.classList.remove('ativo');
+                }
+            });
+        }
+
+        window.addEventListener('scroll', exibirSecao)
+    }
+}
+
+animacaoAoScroll();
